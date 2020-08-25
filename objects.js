4131393c9e9a75e53a32075f7fc4983c97befcf8
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-25 20:52:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,7 @@
 var playlist = { "Slowdive": "Alison",
 "My Bloody Valentine": "Sometimes"}
 
-function updatePlaylist(playlist, artist, song) {playlist["artist"]="song"; return playlist;}
+function updatePlaylist(playlist, artist, song) {
+  playlist["artist"]="song"; 
+  return playlist;
+}
