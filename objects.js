25825f352a75eac1a3f8d461054242731a437a06
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-25 20:46:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = { "taylor swift": "getaway car",
 "selena gomez": "rare"}
 
-function updatePlaylist(playlist,'justin bieber', 'baby') { playlist['justin bieber']='baby'; return playlist; }
+function updatePlaylist(playlist, justin bieber, baby) { playlist['justin bieber']='baby'; return playlist; }
